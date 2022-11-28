7669a88dc98cda6d3f988866fc737faa4faf2506
Eno Compton <enocom@google.com>
2022-11-27 20:38:00 -0700

    Add note about installing packer

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+--[[
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+--]]
 require('plugins')
 require('settings')
 require('maps')
