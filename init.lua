a806fd800743f58243c5ab9b7f72a2f4954e8a50
Eno Compton <enocom@google.com>
2022-11-30 13:13:29 -0700

    Get rid of all the trash

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim \
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 --]]
+-- plugins goes first so all settings take effect correctly
 require('plugins')
+
 require('settings')
 require('maps')
