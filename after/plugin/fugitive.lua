56a75f6e18a26073db34116adde39c7ea14a8646
Eno Compton <enocom@google.com>
2023-07-03 16:55:12 -0600

    Update pull rebase remap

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,9 +1,9 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
-vim.keymap.set("n", "<leader>p", function()
+vim.keymap.set("n", "<leader>gp", function()
     vim.cmd.Git('push')
 end)
 
-vim.keymap.set("n", "<leader>P", function()
-    vim.cmd.Git({"pull", "--rebase"})
+vim.keymap.set("n", "<leader>gP", function()
+    vim.cmd.Git({ "pull", "--rebase" })
 end)
