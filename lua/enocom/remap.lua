c706529ecc3f901dd221c448b33065072b3bff84
Eno Compton <enocom@google.com>
2023-07-03 09:48:57 -0600

    Big refactor and new plugins

---
+++ lua/enocom/remap.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+
+-- Make it easy to edit and source configuration
+vim.keymap.set('n', '<leader>ev', ':edit $MYVIMRC<CR>')
+vim.keymap.set('n', '<leader>ee', ':source $MYVIMRC<CR>')
+
+-- Make it easy to switch splits
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Keymap to get to Ex mode
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Manually format buffer, don't do it automatically
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format()
+end)
+
+-- make it easy to yank to vim or system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Keymaps I'm not sure I want
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
