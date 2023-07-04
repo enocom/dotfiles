31a33571258204d49eb7be750bbf4c40e76a8073
Eno Compton <enocom@google.com>
2023-07-04 09:24:58 -0600

    Remove unused remaps

--- nvim/lua/enocom/remap.lua
+++ nvim/lua/enocom/remap.lua
@@ -7,19 +7,7 @@ vim.keymap.set('n', '<leader>ee', ':source $MYVIMRC<CR>')
 -- Keymap to get to Ex mode
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Manually format buffer, don't do it automatically
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 -- make it easy to yank to vim or system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
-
--- Keymaps I'm not sure I want
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
