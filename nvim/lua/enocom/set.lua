c5c75c0c7f45074b83fa97456edff19283438230
Eno Compton <enocom@google.com>
2023-07-04 09:30:48 -0600

    Clean up setings

--- nvim/lua/enocom/set.lua
+++ nvim/lua/enocom/set.lua
@@ -1,44 +1,47 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor      = ""
 
-vim.opt.number = true
+vim.opt.number         = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop        = 4
+vim.opt.softtabstop    = 4
+vim.opt.shiftwidth     = 4
+vim.opt.expandtab      = true
 
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.splitbelow    = true
-vim.opt.splitright    = true
+vim.opt.smartcase      = true
+vim.opt.smartindent    = true
+vim.opt.splitbelow     = true
+vim.opt.splitright     = true
 
-vim.opt.wrap = false
+vim.opt.wrap           = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.writebackup   = false
+vim.opt.swapfile       = false
+vim.opt.backup         = false
+vim.opt.writebackup    = false
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+vim.opt.hlsearch       = false
+vim.opt.incsearch      = true
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors  = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.signcolumn     = "yes"
 
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.updatetime     = 50
+vim.opt.colorcolumn    = "80"
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-  end
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.code_action({
+            context = {
+                only = { 'source.organizeImports' }
+            },
+            apply = true
+        })
+    end
 })
