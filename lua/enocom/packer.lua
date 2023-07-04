eb897782583371ceaca21db881dfe288907569a0
Eno Compton <enocom@google.com>
2023-07-03 22:32:31 -0600

    Add autocomplete for neovim lua

--- lua/enocom/packer.lua
+++ lua/enocom/packer.lua
@@ -43,6 +43,7 @@ return require('packer').startup(function(use)
             { 'hrsh7th/nvim-cmp' },     -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
+            { 'hrsh7th/cmp-nvim-lua' }, -- source for neovim Lua API.
         }
     })
 
