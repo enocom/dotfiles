a3ca9631f9e52bf12b9ddf25bc3a4ff881f8fcfc
Eno Compton <enocom@google.com>
2023-12-15 10:57:05 -0700

    Flatten after directory too

--- nvim/init.lua
+++ nvim/init.lua
@@ -132,3 +132,74 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 vim.g.hardtime_default_on = 1
 
 vim.g.netrw_banner        = 0
+
+local lsp = require("lsp-zero").preset({
+    name = "recommended",
+    float_border = 'single',
+})
+
+lsp.on_attach(function(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+
+lsp.ensure_installed({
+    'gopls',
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
+})
+
+lsp.configure('lua_ls', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+lsp.setup()
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({ previewer = false })
+end, {})
+
+vim.keymap.set('n', '<C-p>', function()
+    builtin.git_files({ previewer = false })
+end, {})
+
+vim.cmd.colorscheme('tokyonight-night')
+
+require('nvim-treesitter.configs').setup {
+    -- A list of parser names, or "all"
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "go",
+        "lua",
+        "rust",
+        "zig",
+    },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
