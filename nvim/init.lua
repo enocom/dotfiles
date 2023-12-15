f9c9fc96a652341745ea5f485e08c8e28a9b3838
Eno Compton <enocom@google.com>
2023-12-15 11:01:40 -0700

    Cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,34 +79,27 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 -- format manually because something is broken with auto
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.opt.guicursor      = ""
+vim.g.netrw_banner        = 0
 
+vim.opt.guicursor      = ""
 vim.opt.number         = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop        = 4
 vim.opt.softtabstop    = 4
 vim.opt.shiftwidth     = 4
 vim.opt.expandtab      = true
-
 vim.opt.smartcase      = true
 vim.opt.smartindent    = true
 vim.opt.splitbelow     = true
 vim.opt.splitright     = true
-
 vim.opt.wrap           = false
-
 vim.opt.swapfile       = false
 vim.opt.backup         = false
 vim.opt.writebackup    = false
-
 vim.opt.hlsearch       = true
 vim.opt.incsearch      = true
-
 vim.opt.termguicolors  = true
-
 vim.opt.signcolumn     = "yes"
-
 vim.opt.updatetime     = 50
 vim.opt.colorcolumn    = "80"
 
@@ -127,11 +120,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end
 })
 
--- takac/vim-hardtime
--- helps break bad habits
-vim.g.hardtime_default_on = 1
-
-vim.g.netrw_banner        = 0
 
 local lsp = require("lsp-zero").preset({
     name = "recommended",
@@ -193,13 +181,7 @@ require('nvim-treesitter.configs').setup {
     auto_install = true,
 
     highlight = {
-        -- `false` will disable the whole extension
         enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
 }
