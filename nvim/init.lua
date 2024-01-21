f996e23f809e3e03495399e85b8f5d3b8c90a3e4
Eno Compton <enocom@google.com>
2024-01-20 22:58:36 -0700

    Tighten up treesitter config

--- nvim/init.lua
+++ nvim/init.lua
@@ -182,22 +182,16 @@ end, {})
 vim.cmd.colorscheme('tokyonight')
 
 require('nvim-treesitter.configs').setup {
-    -- A list of parser names, or "all"
+    auto_install = false,
+
     ensure_installed = {
-        "javascript",
-        "typescript",
         "go",
         "lua",
         "rust",
-        "zig",
     },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
 
     highlight = {
         enable = true,
